ee8f24bac94bfa1df13b57a40ef949c18e6856d9
Bnzxccc <nguy1281@msu.edu>
2024-12-27 18:46:32 -0500

    Update data_processing.py

--- scripts/data_processing.py
+++ scripts/data_processing.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from tensorflow import keras
 import os
 import random
+import tensorflow_io as tfio
 
 class MapFunction:
   def __init__(self, imageSize):
@@ -11,7 +12,8 @@ class MapFunction:
   def decode_resize(self, filePath):
     # transform the image by path
     raw_image = tf.io.read_file(filePath)
-    image = tf.image.decode_image(raw_image, channels=3)
+    image = tfio.experimental.image.decode_tiff(raw_image)
+    iimage = image[..., :3]
     image = tf.image.resize(image, self.imageSize)
     image = tf.ensure_shape(image, (self.imageSize[0], self.imageSize[1], 3))
 
